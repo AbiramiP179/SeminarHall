f3d6e871f66e8689a331f26b7434d956151503ab
unknown <arlynsneha@gmail.com>
2020-09-15 00:34:29 +0530

    bookdb

--- src/Bookingform.js
+++ src/Bookingform.js
@@ -1,8 +1,9 @@
 import React,{Component} from 'react';
 import './BookingForm.css';
+import Axios from 'axios';
 
 const hheading ={
-		width: '300px',
+    width: '300px',
         border: '1px solid #d6d7da',
         padding: '0px 15px 15px 15px',
         borderRadius: '5px',
@@ -12,186 +13,126 @@ const hheading ={
         fontSize: '14px',
         background: '#ffffff',
         margin: '100px auto'
-	}
+  }
 const lllabel={
-	display: 'block',
+  display: 'block',
   //margin-bottom: 10px;
   width: '90%'
-}	
+} 
 const iiinput={
-	display: 'block',
+  display: 'block',
   //margin-bottom: 10px;
      width: '90%',
      padding: '10px',
      border: 'solid 1px #BDC7D8'
 }
 const bbbutton={
-	 backgroundColor: '#00BFFF',
+   backgroundColor: '#00BFFF',
      borderColor: '#3ac162',
      fontWeight: 'bold',
      padding: '12px 15px',
      color: '#ffffff'
 }
 const eerrormessage={
-	color: '#cc0000',
+  color: '#cc0000',
     marginBottom: '12px'
 }
 
-class Bookingform extends Component{
-	 constructor() {
+class Bookingform extends Component {
+   constructor() {
       super();
       this.state = {
-        fields: {},
-        errors: {}
+ 
+      fromdate:"",
+      todate:"",
+      seminarhall:"",
+      purposeofevent:"",
+      numberofpersons:" "
       }
 
-      //this.handleChange = this.handleChange.bind(this);
-      //this.submituserRegistrationForm = this.submituserRegistrationForm.bind(this);
-
+     
     };
-	handlechange =event=>{
-		event.preventDefault()
-		let fields = this.state.fields;
-       fields[event.target.name] = event.target.value;
-       this.setState({
-        fields
-        });
 
-	}
-	submituserbookingForm=event=> {
+ 
+  
+
+  submituserBookingForm=async(event)=> {
       event.preventDefault();
-      if (this.validatebookingForm()) {
-          let fields = {};
-          fields["FromDate"] = "";
-          fields["ToDate"] = "";
-          fields["Session"] = "";
-          fields["SeminarHall"] = "";
-          fields["NumberofPersons"] = "";
-          fields["PurposeofEvent"] = "";
-          //fields["Session"] = "";
-          this.setState({fields:fields});
-          alert("Form submitted");
+     
+      try {
+      var signupRes = await Axios.post("http://localhost:5000/book",{ 
+       seminarhall:this.state.seminarhall,
+      purposeofevent:this.state.purposeofevent,
+      numberofpersons:this.state.numberofpersons
+    });
+      window.alert("Successfully request sent");
+      this.props.history.push("/bookings");
+    } catch (err) {
+      window.alert(err.response.data);
+    } 
       }
+  
+   
+  render()
+  {
+    return (
 
-    }
-    validatebookingForm() {
-
-      let fields = this.state.fields;
-      let errors = {};
-      let formIsValid = true;
-
-      if (!fields["FromDate"]) {
-        formIsValid = false;
-        errors["FromDate"] = "*Please enter FromDate.";
-      }
-       if (!fields["ToDate"]) {
-        formIsValid = false;
-        errors["ToDate"] = "*Please enter ToDate.";
-      }
-      if (!fields["Session"]) {
-        formIsValid = false;
-        errors["Session"] = "*Please select Session.";
-      }
-      if (!fields["SeminarHall"]) {
-        formIsValid = false;
-        errors["SeminarHall"] = "*Please select SeminarHall.";
-      }
-
-      
-
-      if (!fields["NumberofPersons"]) {
-        formIsValid = false;
-        errors["NumberofPersons"] = "*Please enter NumberofPersons.";
-      }
-       if (typeof fields["NumberofPersons"] !== "undefined") {
-        if (!fields["NumberofPersons"].match(/^[0-9]{10}$/)) {
-          formIsValid = false;
-          errors["NumberofPersons"] = "*Please enter only Numerals.";
-        }
-      }
-      if (!fields["PurposeofEvent"]) {
-        formIsValid = false;
-        errors["PurposeofEvent"] = "*Please enter your PurposeofEvent.";
-      }
-
-      if (typeof fields["PurposeofEvent"] !== "undefined") {
-        if (!fields["PurposeofEvent"].match(/^[a-zA-Z ]*$/)) {
-          formIsValid = false;
-          errors["PurposeofEvent"] = "*Please enter alphabet characters only.";
-        }
-      }
-
-      this.setState({
-        errors: errors
-      });
-      return formIsValid;
-  }
-	
-	//changeusername= event =>{
-		//this.setState({
-			//UserName: event.target.value
-		//})
-	//}
-	//changePassword= event =>{
-		//this.setState({
-			//Password: event.target.value
-		//})
-	//}
-	//handlesubmit = event => {
-		//this.setState({
-			//alert (`$ {this.state.UserName}`)
-			//event.preventDefault()
-
-		//})
-	//}
-	
-	render()
-	{
-		return (
-
-		 <div class="bg">	
+     <div class="bg"> 
          <article class="mw6 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10 bnn">
-			
+      
           <h3>Requirements</h3>
-          <form name="userbookingForm"  onSubmit= {this.submituserbookingForm} >
+          <form name="userbookingForm"  onSubmit= {this.submituserBookingForm} >
           <label style={lllabel}>From Date:</label>
-          <input style={iiinput} type="Date" name="FromDate" value={this.state.fields.FromDate} onChange={this.handleChange}  />
-          <div style={eerrormessage} className="errorMsg">{this.state.errors.FromDate}</div>
+          <input style={iiinput} type="Date" name="FromDate" onChange={(e) =>this.setState({
+                              formdate: e.target.value
+                            })}  />
+          
           <label style={lllabel}>ToDate:</label>
-          <input style={iiinput} type="Date" name="ToDate" value={this.state.fields.ToDate} onChange={this.handleChange}  />
-          <div style={eerrormessage} className="errorMsg">{this.state.errors.ToDate}</div>
+          <input style={iiinput} type="Date" name="ToDate" onChange={(e) =>this.setState({
+                              todate: e.target.value
+                            }) } />
+          
           <label style={lllabel}>Session:
-           <select style={iiinput} name="Session" value={this.state.fields.Session} onChange={this.handleChange}>
+           <select style={iiinput} name="Session"  onChange= {(e) =>this.setState( {
+                              session: e.target.value
+                            })}>
               <option value="Full day">Full day</option>
               <option value="Morning">Morning</option>
               <option value="Afternoon">Afternoon</option>
               <option value="Evening">Evening</option>
             </select></label>
-            <div style={eerrormessage} className="errorMsg">{this.state.errors.Session}</div>
+           
             <label style={lllabel}>Seminar Hall:
-           <select style={iiinput} name="SeminarHall" value={this.state.fields.SeminarHall} onChange={this.handleChange}>
+           <select style={iiinput} name="SeminarHall" onChange={(e) =>this.setState({
+                              seminarhall: e.target.value
+                            })}>
               <option value="CSE Seminar Hall">CSE Seminar Hall</option>
-              <option value="Civil Seminar Hall">Civil Seminar Hall</option>
+              <option value="Civil">Civil</option>
               <option value="EEE seminar Hall">EEE seminar Hall</option>
               <option value="ECE Seminar Hall">ECE Seminar Hall</option>
               <option value="Mechanical Seminar Hall">Mechanical Seminar Hall</option>
               <option value="IT seminar Hall">IT seminar Hall</option>
               <option value="Architecture Seminar Hall">Architecture Seminar Hall</option>
             </select></label>
-            <div style={eerrormessage} className="errorMsg">{this.state.errors.SeminarHall}</div>
+           
           <label style={lllabel}>Number of Persons:</label>
-          <input style={iiinput} type="text" name="NumberofPersons" value={this.state.fields.NumberofPersons} onChange={this.handleChange} />
-          <div style={eerrormessage} className="errorMsg">{this.state.errors.NumberofPersons}</div>
+          <input style={iiinput} type="text" name="NumberofPersons"  onChange={(e) =>this.setState({
+                              numberofpersons: e.target.value
+                            })
+                          } />
+         
           <label style={lllabel}>Purpose of The Event:</label>
-          <input style={iiinput} type="text" name="PurposeofEvent" value={this.state.fields.PurposeofEvent} onChange={this.handleChange} />
-          <div style={eerrormessage}className="errorMsg">{this.state.errors.PurposeofEvent}</div>
+          <input style={iiinput} type="text" name="PurposeofEvent"  onChange={(e) =>this.setState({
+                              purposeofevent: e.target.value
+                            })} />
+          
 
           <button class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit">Check Availability</button>
           </form>
          
     </article>
   </div>
-			);
-	}
-}	
+      );
+  }
+}
 export default Bookingform;
